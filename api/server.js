ea271dff7294b5a35a85fb9fb13e588c34f558d5
D-cyber-web <muthama3david@gmail.com>
2023-10-23 14:43:21 +0300

    created client

--- api/server.js
+++ api/server.js
@@ -16,13 +16,13 @@ mongoose.connect("mongodb://127.0.0.1:27017/mern-todo", {
 
 const Todo = require('./models/Todo');
 
-app.get('/todos', async(req, res) =>{
+app.get('/todos', async(req, res) => {
     const todos = await Todo.find();
 
     res.json(todos);
 });
 
-app.post('/todos/new', (req, res) =>{
+app.post('/todos/new', (req, res) => {
     const todo = new Todo({
         text: req.body.text
     });
@@ -36,6 +36,16 @@ app.delete('/todo/delete/:id', async(req, res) => {
     const result = await Todo.findByIdAndDelete(req.params.id);
     
     res.json(result);
-});
+})
 
-app.listen(3001, () => console.log("server stared on port 3001")); 
+app.put('/todo/complete/:id', async(req, res) => {
+    const result = await Todo.findById(req.params.id);
+
+    todo.complete = !todo.complete;
+    
+    todo.save();
+
+    res.json(result);
+})
+
+app.listen(3001, () => console.log("server stared on port 3001"));  
